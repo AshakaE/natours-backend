ed015a4e7e902045535dbe4cf449c94e9f7587b1
AshakaE <ashakaegerega@gmail.com>
2021-08-05 13:09:23 +0100

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(
+    `${__dirname}/dev-data/data/tours-simple.json`
+  )
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -27,7 +29,10 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
   // if (id > tours.length) {
   if (!tour) {
-    return res.status(404).json({ status: 'fail', meassage: 'Invalid Id' });
+    return res.status(404).json({
+      status: 'fail',
+      meassage: 'Invalid Id',
+    });
   }
 
   res.status(200).json({
@@ -40,23 +45,48 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.assign(
+    { id: newId },
+    req.body
+  );
 
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
     }
   );
 });
 
 app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', meassage: 'Invalid Id' });
+    return res.status(404).json({
+      status: 'fail',
+      meassage: 'Invalid Id',
+    });
   }
-  res.status(200).json({ status: 'success', data: { tour: 'Updated' } });
+  res.status(200).json({
+    status: 'success',
+    data: { tour: 'Updated' },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      meassage: 'Invalid Id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 const port = 5914;
