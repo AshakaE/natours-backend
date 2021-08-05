398981d90ca069693512e207759a4cefc17be93e
AshakaE <ashakaegerega@gmail.com>
2021-08-05 16:34:17 +0100

    Environment variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 5914;
+const port = process.env.PORT || 5914;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
