554c8a53d8bbfe190dc89e24e8fc55fd487bce61
AshakaE <ashakaegerega@gmail.com>
2021-08-17 21:09:30 +0100

    connect db to mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const port = process.env.PORT || 5914;
 app.listen(port, () => {
