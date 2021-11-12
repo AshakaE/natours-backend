904f91982b1e249ad4f432c28287e7e4524ffa8c
AshakaE <ashakaegerega@gmail.com>
2021-11-12 22:45:31 +0100

    Update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
-const User = require('../models/userModel');
+const User = require('../models/userModel')
 // const APIFeatures = require('../utils/apiFeatures');
-const catchAsync = require('../utils/catchAsync');
-// const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el]
+    }
+  })
+  return newObj
+}
 
 exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
+  const users = await User.find()
 
   // SEND RESPONSE
   res.status(200).json({
@@ -13,33 +23,56 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     data: {
       users,
     },
-  });
-});
+  })
+})
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    )
+  }
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  })
+})
 
 exports.getUser = catchAsync(async (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'Undefined',
-  });
-});
+  })
+})
 
 exports.createUser = catchAsync(async (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'Undefined',
-  });
-});
+  })
+})
 
 exports.updateUser = catchAsync(async (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'Undefined',
-  });
-});
+  })
+})
 
 exports.deleteUser = catchAsync(async (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'Undefined',
-  });
-});
+  })
+})
