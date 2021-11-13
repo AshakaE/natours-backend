1ca15636cac70c1ee4fe1b0e7bf2139c1989d051
AshakaE <ashakaegerega@gmail.com>
2021-11-13 09:37:27 +0100

    Implement rate limiting

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-const app = express();
+const app = express()
 
-//MIDDLEWARE
+//GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
-app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`));
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+})
+
+app.use('/api', limiter)
+
+app.use(express.json())
+
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
 // ROUTE HANDLERS
 
@@ -30,13 +40,13 @@ app.use((req, res, next) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 // ROUTES
-app.use('/api/v1/tours/', tourRouter);
-app.use('/api/v1/users/', userRouter);
+app.use('/api/v1/tours/', tourRouter)
+app.use('/api/v1/users/', userRouter)
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-});
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 
-module.exports = app;
+module.exports = app
