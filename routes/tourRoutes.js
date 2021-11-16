11811c424105b04d7ae203a8d8d5a3b5e1630632
AshakaE <ashakaegerega@gmail.com>
2021-11-16 13:08:31 +0100

    Nested routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const tourController = require('../controllers/tourController')
 const authController = require('../controllers/authController')
-const reviewController = require('../controllers/reviewController')
+const reviewRouter = require('./../routes/reviewRoutes')
 
 const router = express.Router()
 
-// router.param('id', tourController.checkID);
+// Mounting a router in tour routes
+router.use('/:tourId/reviews', reviewRouter)
 
 //MIDDLEWARE
 router
@@ -27,14 +28,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  )
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   )
 
 module.exports = router
