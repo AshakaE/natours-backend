3c23033d0c80e1bed94f997198252139808d352e
AshakaE <ashakaegerega@gmail.com>
2021-11-18 11:38:37 +0100

    Add a /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,6 +14,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj
 }
 
+exports.getMe = catchAsync(async (req, res, next) => {
+  req.params.id = req.user.id
+  next()
+})
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(
