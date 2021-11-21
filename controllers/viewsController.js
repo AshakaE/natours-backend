4deee0540e429f201ff2b4aba5253bb83917c172
AshakaE <ashakaegerega@gmail.com>
2021-11-21 13:50:31 +0100

    Add tour overview card

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.getOveriew = (req, res) => {
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
+
+exports.getOveriew = catchAsync(async (req, res) => {
+  // Get tour data
+  const tours = await Tour.find()
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   })
-}
+})
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
